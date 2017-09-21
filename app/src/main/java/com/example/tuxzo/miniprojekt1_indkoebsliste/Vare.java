7f6a5c1b63d1d00cec542c63b319976373c534b1
PWM <tbt_paddik@hotmail.com>
2017-09-21 11:25:57 +0200

    Flere ting opdateret

---
+++ app/src/main/java/com/example/tuxzo/miniprojekt1_indkoebsliste/Vare.java
@@ -0,0 +1,42 @@
+package com.example.tuxzo.miniprojekt1_indkoebsliste;
+
+/**
+ * Created by PWM on 21-09-17.
+ */
+
+public class Vare {
+    private String name;
+    private double normalPris;
+    private int mængde;
+
+    public Vare(String name, double normalPris, int mængde) {
+        this.name = name;
+        this.normalPris = normalPris;
+        this.mængde = mængde;
+    }
+
+    public double getNormalPris() {
+        return normalPris;
+    }
+
+    public void setNormalPris(double normalPris) {
+        this.normalPris = normalPris;
+    }
+
+    public int getMængde() {
+        return mængde;
+    }
+
+    public void setMængde(int mængde) {
+        this.mængde = mængde;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
