23b599e02766e398e4715da433a31da40b9b783f
Steen Tingager Kristensen <steentingager@gmail.com>
2017-09-21 13:49:36 +0200

    outcomment

--- app/src/main/java/com/example/tuxzo/miniprojekt1_indkoebsliste/MainActivity.java
+++ app/src/main/java/com/example/tuxzo/miniprojekt1_indkoebsliste/MainActivity.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.text.Html;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -21,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         getSupportActionBar().setTitle("Liste Over Butikker");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         ListView listView = (ListView) findViewById(R.id.listButikker);
         final ArrayList<Butik> butikker = Storage.createButikker();
@@ -40,5 +44,30 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        registerForContextMenu(listView);
+
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_context, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        switch (item.getItemId()) {
+            case R.id.item_delete:
+                Toast.makeText(this, "Delete", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_edit:
+                Toast.makeText(this, "Edit", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
     }
 }
